3900b783ae50c21a3a2d3b972196bc01b37d58f2
Douglas S. Green <douglas@nurd.site>
2024-03-04 10:54:47 -0500

    Adding first draft

---
+++ bin/check.php
@@ -0,0 +1,42 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * Check URLs.
+ * To do:
+ * 1. Decide whether to download HTML/Text file body or skip for PDF/etc.
+ * 2. Skip data links.
+ * 3. Only follow URLs for internal links (isInternal).
+ * 4. Allow retries.
+ * 5. Produce report.
+ */
+
+use LinkChecker\Url;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+if ($argc != 2) {
+    die("Usage: " . basename($argv[0]) . " CONFIG_FILE\n");
+}
+
+if (!file_exists($argv[1])) {
+    die("File not found\n");
+}
+
+$config = parse_ini_file($argv[1], true);
+
+$links = $config['links']['link'] ?? [];
+if (!$links) {
+    die("No links to check\n");
+}
+
+$deleteParams = $config['delete_params']['param'] ?? [];
+
+Url::setDomains($links);
+
+Url::deleteParams($deleteParams);
+
+foreach ($links as $link) {
+    $url = new Url($link);
+    var_dump($url);
+}
